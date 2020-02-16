d4308612d3a2589c582d3216bc4957f2c952d987
bbo51dog <bbo51@icloud.com>
2020-02-16 21:25:04 +0900

    Repository

--- src/main/kotlin/net/bbo51dog/ecokkit/EcokkitPlugin.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/EcokkitPlugin.kt
@@ -1,7 +1,15 @@
 package net.bbo51dog.ecokkit
 
 import cn.nukkit.plugin.PluginBase
+import net.bbo51dog.ecokkit.repository.RepositoryProvider
 
 class EcokkitPlugin : PluginBase() {
 
+    private lateinit var RepositoryProvider: provider
+
+    override fun onEnable(){
+        val folder = this.getDataFolder()
+        folder.mkdir()
+        this.provider = RepositoryProvider(folder.absolutePath + "/ecokkit.db ")
+    }
 }
--- src/main/kotlin/net/bbo51dog/ecokkit/repository/RepositoryProvider.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/repository/RepositoryProvider.kt
@@ -1,7 +1,19 @@
 package net.bbo51dog.ecokkit.repository
 
+import cn.nukkit.utils.MainLogger
+import java.sql.*
 import net.bbo51dog.ecokkit.repository.sqlite.SQLiteUserRepository
 
 class RepositoryProvider{
 
+    private lateinit var Connection: connection
+
+    constructor(path: String): this(){
+        try{
+            Class.forName("org.sqlite.JDBC")
+            val connection = DriverManager.getConnection("jdbc:sqlite:$path")
+        }catch(exception: SQLException){
+            MainLogger.getLogger().logException(exception)
+        }
+    }
 }
