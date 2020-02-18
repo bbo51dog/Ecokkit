3dead3b3dabf10fda4ad94a22edafcbb5e9ea441
bbo51dog <bbo51@icloud.com>
2020-02-18 19:57:21 +0900

    UserRepository methods

--- src/main/kotlin/net/bbo51dog/ecokkit/repository/UserRepository.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/repository/UserRepository.kt
@@ -1,5 +1,14 @@
 package net.bbo51dog.ecokkit.repository
 
+import net.bbo51dog.ecokkit.user.User
+
 interface UserRepository : Repository {
 
+    fun registerUser(user: User)
+
+    fun getUser(xuid: String)
+
+    fun updateUser(user: User)
+
+    fun existsUserId(xuid: String)
 }
