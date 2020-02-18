7a73a130f661acf52e45e4e1a3bc4e26e372ee6d
bbo51dog <bbo51@icloud.com>
2020-02-18 22:26:15 +0900

    IEcokkit

--- src/main/kotlin/net/bbo51dog/ecokkit/api/IEcokkitAPI.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/api/IEcokkitAPI.kt
@@ -2,4 +2,25 @@ package net.bbo51dog.ecokkit.api
 
 interface IEcokkitAPI {
 
+    val unit: String
+
+    val default: Int
+
+    fun getMoney(xuid: String): Int
+
+    fun setMoney(xuid: String, money: Int)
+
+    fun addMoney(xuid: String, money: Int)
+
+    fun reduceMoney(xuid: String, money: Int)
+
+    fun getMoneyByName(name: String): Int
+
+    fun setMoneyByName(name: String, money, Int)
+
+    fun addMoneyByName(name: String, money: Int)
+
+    fun reduceMoneyByName(name: String, money: Int)
+
+    fun createMoneyData(xuid: String, name: String)
 }
