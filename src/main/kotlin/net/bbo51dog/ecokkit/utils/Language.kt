2ecc47007b5774e9410a023ba250bc9b27bcef3b
bbo51dog <bbo51@icloud.com>
2020-02-21 18:37:30 +0900

    get message from　Language

--- src/main/kotlin/net/bbo51dog/ecokkit/utils/Language.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/utils/Language.kt
@@ -5,4 +5,13 @@ class Language(map: Map<String, Any>) {
     private val map = map
     
     val prefix: String = map["prefix"].toString()
+    
+    fun getMessage(key: String, prefix: Boolean = true): String {
+        val message = if (prefix) {
+            this.prefix + this.map[key]
+        } else {
+            this.map[key]
+        }
+        return message.toString()
+    }
 }
