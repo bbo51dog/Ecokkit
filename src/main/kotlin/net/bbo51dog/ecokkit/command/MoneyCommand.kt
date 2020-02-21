9cbff854da6c6b1d270a7cb987133113063d54a4
bbo51dog <bbo51@icloud.com>
2020-02-21 19:09:42 +0900

    Update money command

--- src/main/kotlin/net/bbo51dog/ecokkit/command/MoneyCommand.kt
+++ src/main/kotlin/net/bbo51dog/ecokkit/command/MoneyCommand.kt
@@ -1,11 +1,45 @@
 package net.bbo51dog.ecokkit.command
 
+import cn.nukkit.Player
 import cn.nukkit.command.Command
 import cn.nukkit.command.CommandSender
+import net.bbo51dog.ecokkit.api.EcokkitAPI
 
 class MoneyCommand : Command("money", "Ecokkit money command", "/money []") {
 
+    init {
+        this.permission = "ecokkit.command.money"
+    }
+
     override fun execute(sender: CommandSender?, commandLabel: String?, args: Array<out String>?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (sender == null) {
+            return false
+        }
+        val api = EcokkitAPI.instance
+        val lang = api.language
+        if (sender !is Player) {
+            sender.sendMessage(lang.getMessage("sender.not.player"))
+            return false
+        }
+        val xuid = sender.loginChainData.xuid
+        if (!api.exists(xuid)) {
+            api.createMoneyData(xuid, sender.name)
+        }
+        val size = args?.size
+        if (size == null) {
+            sender.sendMessage(this.usage)
+            return false
+        }
+        if (size < 1) {
+            sender.sendMessage(this.usage)
+            return false
+        }
+        when (args[0]) {
+            else -> {
+                sender.sendMessage(this.usage)
+                return false
+            }
+        }
+        return true
     }
 }
